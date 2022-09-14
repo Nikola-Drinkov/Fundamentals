561339346daf90f0489d4b3b15a6ac6a331b4595
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-09-14 21:11:43 +0300

    Solved problem

---
+++ src/BasicSyntax/MultiplicationTable_10.java
@@ -0,0 +1,16 @@
+package BasicSyntax;
+
+import java.util.Scanner;
+
+public class MultiplicationTable_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+        int result = 0;
+        for(int i=1; i<=10; i++){
+            result=n*i;
+            System.out.printf("%d X %d = %d%n",n,i,result);
+        }
+    }
+}

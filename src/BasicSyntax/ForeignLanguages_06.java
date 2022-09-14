e15b25c6477bdfb4470a168c1685c4da1eca89e8
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-09-14 20:16:52 +0300

    Solved problem

---
+++ src/BasicSyntax/ForeignLanguages_06.java
@@ -0,0 +1,23 @@
+package BasicSyntax;
+
+import java.util.Scanner;
+
+public class ForeignLanguages_06 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String country = sc.nextLine();
+        String language = "";
+        if(country.equals("England")||country.equals("USA")){
+            language="English";
+        }
+        else if(country.equals("Spain")||country.equals("Argentina")||country.equals("Mexico")){
+            language="Spanish";
+        }
+        else{
+            language = "unknown";
+        }
+        System.out.println(language);
+    }
+}

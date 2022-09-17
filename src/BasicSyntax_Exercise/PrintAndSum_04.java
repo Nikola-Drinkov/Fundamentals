d12dd1f06b547a32f190dcc77dab9bf68fae5587
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-09-17 22:29:16 +0300

    Solved problem

---
+++ src/BasicSyntax_Exercise/PrintAndSum_04.java
@@ -0,0 +1,20 @@
+package BasicSyntax_Exercise;
+
+import java.util.Scanner;
+
+public class PrintAndSum_04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int start = Integer.parseInt(sc.nextLine());
+        int end = Integer.parseInt(sc.nextLine());
+        int sum =0;
+
+        for (int i=start; i<=end; i++){
+            System.out.print(i+" ");
+            sum+=i;
+        }
+        System.out.println();
+        System.out.printf("Sum: %d",sum);
+    }
+}

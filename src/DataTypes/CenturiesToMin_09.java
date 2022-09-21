d1b511d913379acd5895684fa9e7cc154160797b
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-09-21 21:34:41 +0300

    Solved problem

---
+++ src/DataTypes/CenturiesToMin_09.java
@@ -0,0 +1,17 @@
+package DataTypes;
+
+import java.util.Scanner;
+
+public class CenturiesToMin_09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        byte centuries = Byte.parseByte(sc.nextLine());
+        int years = centuries*100;
+        double days = years*365.2422;
+        double hours = days*24;
+        double minutes = hours*60;
+
+        System.out.printf("%d centuries = %d years = %.0f days = %.0f hours = %.0f minutes",centuries,years,days,hours,minutes);
+    }
+}

9034446119ae72076ccdf943018b8359c9893a9b
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-09-28 21:06:26 +0300

    Solved problem

---
+++ src/Arrays/ReverseStringArr_03.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ReverseStringArr_03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String [] arr = sc.nextLine().split(" ");
+
+        for(int i=0; i<arr.length/2; i++){
+            String temp = arr[i];
+            arr[i] = arr[arr.length-1-i];
+            arr[arr.length-1-i]=temp;
+        }
+        System.out.print(String.join(" ",arr));
+    }
+}

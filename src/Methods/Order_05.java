2d15e3965c3e7eff98c25d66aa1abb845f795880
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-10-06 20:18:42 +0300

    Solved problem

---
+++ src/Methods/Order_05.java
@@ -0,0 +1,30 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class Order_05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String product = sc.nextLine();
+        int quantity = Integer.parseInt(sc.nextLine());
+        System.out.println(makePrice(product,quantity));
+    }
+    public static double makePrice(String product, int quantity){
+        double price = 0;
+        switch (product){
+            case"coffee":
+                price = 1.50;
+                break;
+            case"water":
+                price = 1.00;
+                break;
+            case"coke":
+                price = 1.40;
+                break;
+            case"snacks":
+                price = 2.00;
+                break;
+        }
+        return price*quantity;
+    }
+}

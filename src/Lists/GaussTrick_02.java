b4d4a1949f7da3b04a1e614ae7c8728c0c3cc239
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-10-12 20:54:07 +0300

    Solved problem

---
+++ src/Lists/GaussTrick_02.java
@@ -0,0 +1,24 @@
+package Lists;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class GaussTrick_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Integer> numbersList = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        int size = numbersList.size();
+        for (int i = 0; i < size/2 ; i++) {
+            int firstNum = numbersList.get(i);
+            int secondNum = numbersList.get(numbersList.size()-1);
+            numbersList.set(i, firstNum+secondNum);
+            numbersList.remove(numbersList.size()-1);
+        }
+        for (int item:numbersList) {
+            System.out.print(item+" ");
+        }
+    }
+}

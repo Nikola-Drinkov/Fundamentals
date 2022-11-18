1bb100e55e0777b3761ed2cb473c9779e98c6717
Nikola-Drinkov <nikola13drinkov@gmail.com>
2022-11-18 22:12:45 +0200

    Solved problem

---
+++ src/RegularExpressions/MatchDates_03.java
@@ -0,0 +1,21 @@
+package RegularExpressions;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchDates_03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        String regex = "(?<day>[0-9]{2})([\\.\\-\\/])(?<month>[A-Z][a-z]{2})\\2(?<year>[0-9]{4})";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        while(matcher.find()){
+            String day = matcher.group("day");
+            String month = matcher.group("month");
+            String year = matcher.group("year");
+            System.out.printf("Day: %s, Month: %s, Year: %s%n",day,month,year);
+        }
+    }
+}
